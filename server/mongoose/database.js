1dd8d164a1b2022cf0de12bee72e7d91498eca9c
Moulaye dagnon <moulayedagnon@gmail.com>
2025-10-17 10:24:53 +0000

    fixed error

--- server/mongoose/database.js
+++ server/mongoose/database.js
@@ -5,4 +5,4 @@ module.exports = mongoose
     "mongodb+srv://moulayedagnon_db_user:moul%409460@cluster-1.7c0u04v.mongodb.net/entertainment-app"
   )
   .then(() => console.log("mongodb Connected!"))
-  .catch((e) => console.log("e"));
+  .catch((e) => console.log(e));
